da95beeea3f9b9bfbf993f0482845e591b7a3852
Diego Coimbra <diego62713@estudante.ifb.edu.br>
2024-03-21 20:04:59 -0300

    agora

---
+++ Aula8_15_3/Ola.java
@@ -0,0 +1,13 @@
+import util.java.Scanner
+
+public class Ola {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual é o seu nome?");
+        String nome = sc.nextLine();
+        System.out.println("Olá, " + nome + "!");
+    }
+}
+```
+
+```java
