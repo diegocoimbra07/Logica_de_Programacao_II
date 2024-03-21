112b4d58374ccd2b05f2f63ca2a9b1be97f90cad
Diego Coimbra <diego62713@estudante.ifb.edu.br>
2024-03-21 20:45:23 -0300

    20:45

--- Aula8_15_3/Questao3.java
+++ Aula8_15_3/Questao3.java
@@ -4,13 +4,25 @@ digite o número fora do intervalo deverá aparecer inválido. */
 
 package Aula8_15_3;
 
-import java.util.Scanner; // Importa a classe Scanner
-
-class mes{
-    public static int 
+ class Trimestre { //criando a classe
+    public String MesTrimestre(int mes){ // metodo público que recebe inteiro mes
+        switch (mes){ // valor da variável
+            case 1:
+                return "Janeiro";
+            case 2:
+                return "Fevereiro";
+            case 3:
+                return "Março";
+            default:
+                return "Inválido";
+        }
 }
+ 
 
-
-public class Questao3 {
-    
+public static void Questao3(String[] args) {
+    Trimestre trimestre = new Trimestre(); //criando uma instância da classe
+    for (int i = 0; i < 5; i++){ // interar de 0 a 4
+        System.out.println(i + ": " + trimestre.MesTrimestre(i));
+    }
+}
 }
